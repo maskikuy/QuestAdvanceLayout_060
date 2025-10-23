44f81a0e964a7dc6769db7cb880cd2360a54fa70
maskikuy <kproject305@gmail.com>
2025-10-23 11:57:18 +0700

    menambahkan gambar kedalam row

--- app/src/main/java/com/example/pertemuan_4/ui/theme/UIpertama.kt
+++ app/src/main/java/com/example/pertemuan_4/ui/theme/UIpertama.kt
@@ -3,16 +3,21 @@ package com.example.pertemuan_4.ui.theme
 import android.R.attr.text
 import android.app.Activity
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.TextUnit
@@ -37,7 +42,15 @@ fun ActivitasPertama(modifier: Modifier){
             .fillMaxWidth(1f)
             .padding(12.dp),
             colors = CardDefaults.cardColors(
-
-            ))
-    }
+                containerColor = Color.DarkGray
+            )){
+            Row() {
+                val gambar = painterResource(R.drawable.Logo_real)
+                image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier.size(100.dp)
+                )
+            }
+        }
 }

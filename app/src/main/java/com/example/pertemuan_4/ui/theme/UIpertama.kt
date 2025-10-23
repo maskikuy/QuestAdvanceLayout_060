9c2b7a2b3cf37be8e938fe8f94a85aaf9b582f75
maskikuy <kproject305@gmail.com>
2025-10-23 11:48:45 +0700

    menambahkan coloumn

--- app/src/main/java/com/example/pertemuan_4/ui/theme/UIpertama.kt
+++ app/src/main/java/com/example/pertemuan_4/ui/theme/UIpertama.kt
@@ -1,8 +1,43 @@
 package com.example.pertemuan_4.ui.theme
 
+import android.R.attr.text
 import android.app.Activity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pertemuan_4.R
 
 
 @Composable
-fun ActivitasPertama
+fun ActivitasPertama(modifier: Modifier){
+    Column(modifier = Modifier.padding(top=100.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(stringResource(R.string.prodi),
+            fontsize = 35.sp,
+            fontweight = FontWeight.Bold)
+        Text(stringResource( R.string.univ),
+            fontsize = 22.sp)
+        Spacer(modifier = Modifier.height(25.dp))
+        Card (modifier = modifier
+            .fillMaxWidth(1f)
+            .padding(12.dp),
+            colors = CardDefaults.cardColors(
+
+            ))
+    }
+}

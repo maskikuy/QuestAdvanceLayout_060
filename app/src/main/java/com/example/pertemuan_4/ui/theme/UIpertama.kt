07602f718fa96782c8d2dda9f3ca9546a7a2065a
maskikuy <kproject305@gmail.com>
2025-10-23 11:59:00 +0700

    menambahkan spacer 30dp

--- app/src/main/java/com/example/pertemuan_4/ui/theme/UIpertama.kt
+++ app/src/main/java/com/example/pertemuan_4/ui/theme/UIpertama.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -17,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -49,8 +51,12 @@ fun ActivitasPertama(modifier: Modifier){
                 image(
                     painter = gambar,
                     contentDescription = null,
-                    modifier = Modifier.size(100.dp)
+                    modifier = Modifier.size(100.dp).padding(5.dp)
                 )
+                Spacer(modifier = Modifier.width(30.dp))
+                Column() {
+
+                }
             }
         }
 }

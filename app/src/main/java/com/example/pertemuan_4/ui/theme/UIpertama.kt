77a598b72daddfb39e9a2e377195eee2c850febe
maskikuy <kproject305@gmail.com>
2025-10-23 12:22:11 +0700

    menambahkan padding dalam text

--- app/src/main/java/com/example/pertemuan_4/ui/theme/UIpertama.kt
+++ app/src/main/java/com/example/pertemuan_4/ui/theme/UIpertama.kt
@@ -82,6 +82,7 @@ fun ActivitasPertama(modifier: Modifier){
                 stringResource(R.string.copy),
                 modifier = Modifier
                     .align ( Alignment.BottomCenter )
+                    .padding(bottom = 50.dp)
             )
         }
 }
